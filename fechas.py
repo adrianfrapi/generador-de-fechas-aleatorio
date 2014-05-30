2c05093649b32d6c9f1038bb5af7f4346b9e44fc
Adrian Frapiccini <adrianfrapi@users.noreply.github.com>
2014-05-30 16:41:12 -0300

    Update fechas.py

--- fechas.py
+++ fechas.py
@@ -1,2 +1,26 @@
-generador-de-fechas-aleatorio
-=============================
+#!/usr/bin/python
+import random
+f=open("fechas.csv","w")
+for i in range(20):
+	dia=random.randint(1, 31)
+	mes=random.randint(1, 12)
+	anio=random.randint(0, 14)
+	if mes == 2:
+		if dia>=28:
+			dia=28
+	if mes == 4:
+		if dia==31:
+			dia=30
+	if mes == 6:
+		if dia==31:
+			dia=30
+	if mes == 9:
+		if dia==31:
+			dia=30
+	if mes == 11:
+		if dia==31:
+			dia=30
+	fecha=str(dia)+"/"+str(mes)+"/"+str(anio)+";\r"
+	print fecha
+	f.write(fecha)
+f.close()
